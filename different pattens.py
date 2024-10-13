9b008cdff7d3b9640fd20927642561b132e9a596
Aswanth Kuduvan <aswanthk2022@gmail.com>
2024-10-13 17:43:57 +0530

    a new file added

---
+++ different pattens.py
@@ -0,0 +1,60 @@
+# square pattern
+# size = int(input("enter the size of the pattern: "))
+# for i in range(size):
+#     for j in range(size):
+#         print('*', end=' ')
+#     print()
+
+# triangle pattern
+# size = int(input("enter size of the pattern: "))
+# for i in range(size):
+#     for j in range(i+1):
+#         print('*', end=' ')
+#     print()
+
+# reverse triangle
+# size = int(input("enter size of pattern: "))
+# for i in range(size):
+#     for j in range(size-i):
+#         print('*', end=' ')
+#     print()
+
+# right triangle
+# size = int(input("enter size of the pattern: "))
+# for i in range(size):
+#     for j in range(size-i):
+#         print(' ', end=' ')
+#     for k in range(i+1):
+#         print('*',end=' ')
+#     print()
+
+# left triangle
+# size = int(input('enter the size of the pattern: '))
+# for i in range(size):
+#     for j in range(i):
+#         print(' ', end=' ')
+#     for k in range(size-i):
+#         print('*', end=' ')
+#     print()
+
+# mountain pattern
+# size = int(input("enter the size of the pattern: "))
+# for i in range(size):
+#     for j in range(size-i):
+#         print(' ', end=' ')
+#     for k in range(i+1):
+#         print('*', end=' ')
+#     for l in range(k):
+#         print('*', end=' ')
+#     print()
+
+# decreasing mountain
+# size = int(input("enter the size of the pattern: "))
+# for i in range(size):
+#     for j in range(i):
+#         print(' ',end=' ')
+#     for k in range(size-i):
+#         print('*',end=' ')
+#     for l in range(k):
+#         print('*',end=' ')
+#     print()
