1e036ccc9f02a51a6e5468523cf4337e1a37e099
Aswanth Kuduvan <aswanthk2022@gmail.com>
2024-10-17 18:43:15 +0530

    a new file added

---
+++ maximum of two number.py
@@ -0,0 +1,31 @@
+# using function
+
+# def maximum(a, b):
+#     if a > b:
+#         return a
+#     else:
+#         return b
+# num1 = 10
+# num2 = 20
+# print(f'maximum value is {maximum(num1, num2)}')
+
+# using max function
+
+# a = 50
+# b = 20
+# maximum = max(a,b)
+# print(f'maximum value is {maximum}')
+
+# using ternary operator
+
+# a = 20
+# b = 32
+# maximum = a if a > b else b
+# print(f'maximum is {maximum}')
+
+# using lambda
+
+a = 22
+b = 88
+maximum = lambda x,y:x if x > y else y
+print(f'maximum value is {maximum(a,b)}')
