1a102b6bcdc032da3005e3cf7d9fd0dc3603b09a
Aswanth Kuduvan <aswanthk2022@gmail.com>
2024-10-18 17:30:33 +0530

    a new file added

---
+++ odd number using for loop.py
@@ -0,0 +1,15 @@
+numList = [1, 8, 9, 3, 5, 4, 7, 6, 10]
+
+count = 0
+
+for i in numList:
+    if i % 2 == 1 :
+        count += 1
+print(count)
+
+# count = 0
+#
+# for i in range(1,100):
+#     if i % 2 == 1:
+#         count += 1
+# print(count)
